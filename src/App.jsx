71cfb8a08e2312d92f2e7a2482ff54e4d4abc53f
Leo-Marinhoo <leo.ccodes@gmail.com>
2024-11-13 15:46:34 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
